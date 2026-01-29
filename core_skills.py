ac3375624267e8c9ac2e50ae196a635230e58f2e
Collin Mutembei <10899666+collinmutembei@users.noreply.github.com>
2026-01-29 20:49:46 +0300

    add core_skills

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,12 @@
 import random
 # rand_list =
+rand_list = [random.randint(1, 20) for _ in range(10)]
+print(f"rand_list {rand_list}")
 
 # list_comprehension_below_10 =
+below_10 = [x for x in rand_list if x < 10]
+print(f"below 10 {below_10}")
 
 # list_comprehension_below_10 =
+filter_10 = list(filter(lambda n: n < 10, rand_list))
+print(filter_10)
